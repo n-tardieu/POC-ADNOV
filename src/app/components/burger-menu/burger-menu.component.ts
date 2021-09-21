3243d808c6b10292573c31a93d8120ccf6a79b46
Nicolas Tardieu <nicolas.tardieu@altran.com>
2021-09-21 18:48:01 +0200

    feat(nav): add card burger

--- src/app/components/burger-menu/burger-menu.component.ts
+++ src/app/components/burger-menu/burger-menu.component.ts
@@ -1,5 +1,6 @@
 import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-burger-menu',
@@ -9,7 +10,13 @@ import { Component, OnInit } from '@angular/core';
     // Each unique animation requires its own trigger. The first argument of the trigger function is the name
     trigger('rotatedState', [
       state('default', style({ transform: 'rotate(0)' })),
+      state('rotated', style({ transform: 'rotate(180deg)', opacity: 0 })),
+      transition('rotated => default', animate('300ms ease-out')),
+      transition('default => rotated', animate('300ms ease-in'))
+    ]),
+    trigger('rotatedStateClose', [
       state('rotated', style({ transform: 'rotate(180deg)' })),
+      state('default', style({ transform: 'rotate(0)', opacity: 0 })),
       transition('rotated => default', animate('300ms ease-out')),
       transition('default => rotated', animate('300ms ease-in'))
     ])
@@ -17,15 +24,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BurgerMenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit(): void {
   }
+
+  stateChange = true
 
   state: string = 'default';
 
   rotate() {
     this.state = (this.state === 'default' ? 'rotated' : 'default');
   }
-
 }

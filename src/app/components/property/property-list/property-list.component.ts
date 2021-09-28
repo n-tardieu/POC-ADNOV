50986b05e4c3ab6fe117fa4a38bb478c53f1d38c
Nicolas Tardieu <nicolas.tardieu@altran.com>
2021-09-28 16:31:13 +0200

    feat(style): update card list

--- src/app/components/property/property-list/property-list.component.ts
+++ src/app/components/property/property-list/property-list.component.ts
@@ -12,7 +12,22 @@ export class PropertyListComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  propertyList = [{ id: "1", name: "Studio appartement", img: "apprt1.jpg" }, { id: "2", name: "Studio appartement", img: "apprt2.jpg" }, { id: "3", name: "Studio appartement", img: "apprt3.jpg" }, { id: "4", name: "Studio appartement", img: "apprt3.jpg" }, { id: "5", name: "Studio appartement", img: "apprt3.jpg" }]
-  archivedList = [{ id: "4", name: "apprt1", img: "apprt1.jpg" }, { id: "5", name: "apprt1", img: "apprt2.jpg" }, { id: "6", name: "apprt1", img: "apprt3.jpg" }, { id: "4", name: "apprt1", img: "apprt1.jpg" }, { id: "5", name: "apprt1", img: "apprt2.jpg" }, { id: "6", name: "apprt1", img: "apprt3.jpg" }, { id: "4", name: "apprt1", img: "apprt1.jpg" }, { id: "5", name: "apprt1", img: "apprt2.jpg" }]
+  propertyList = [
+    { id: "1", name: "Appartement 2 pièces", location: "St Cyprien - 162 000€" },
+    { id: "2", name: "Appartement 4 pièces", location: "Escalquens - 280 000€" },
+    { id: "3", name: "Appartement 3 pièces", location: "Bourrassol - 188 000€" },
+    { id: "4", name: "Appartement 5 pièces", location: "Balma - 320 000€" },
+    { id: "5", name: "Appartement 1 pièces", location: "Rangueil - 139 000€" }
+  ]
+  archivedList = [
+    { id: "1", name: "Appartement 2 pièces", location: "St Cyprien - 162 000€" },
+    { id: "2", name: "Appartement 4 pièces", location: "Escalquens - 280 000€" },
+    { id: "3", name: "Appartement 3 pièces", location: "Bourrassol - 188 000€" },
+    { id: "4", name: "Appartement 5 pièces", location: "Balma - 320 000€" },
+    { id: "5", name: "Appartement 1 pièces", location: "Rangueil - 139 000€" },
+    { id: "6", name: "Appartement 3 pièces", location: "Bourrassol - 188 000€" },
+    { id: "7", name: "Appartement 5 pièces", location: "Balma - 320 000€" },
+    { id: "8", name: "Appartement 1 pièces", location: "Rangueil - 139 000€" }
+  ]
 
 }

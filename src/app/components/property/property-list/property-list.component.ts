897b60cda8de67e73f7af2f9b449824d0aae9afc
Nicolas Tardieu <nicolas.tardieu@altran.com>
2021-09-28 12:08:08 +0200

    feat(style): update list card

--- src/app/components/property/property-list/property-list.component.ts
+++ src/app/components/property/property-list/property-list.component.ts
@@ -12,7 +12,7 @@ export class PropertyListComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  propertyList = [{ id: "1", name: "Studio appartement", img: "apprt1.jpg" }, { id: "2", name: "Studio appartement", img: "apprt2.jpg" }, { id: "3", name: "Studio appartement", img: "apprt3.jpg" }]
-  archivedList = [{ id: "4", name: "apprt1", img: "apprt1.jpg" }, { id: "5", name: "apprt1", img: "apprt2.jpg" }, { id: "6", name: "apprt1", img: "apprt3.jpg" }]
+  propertyList = [{ id: "1", name: "Studio appartement", img: "apprt1.jpg" }, { id: "2", name: "Studio appartement", img: "apprt2.jpg" }, { id: "3", name: "Studio appartement", img: "apprt3.jpg" }, { id: "4", name: "Studio appartement", img: "apprt3.jpg" }, { id: "5", name: "Studio appartement", img: "apprt3.jpg" }]
+  archivedList = [{ id: "4", name: "apprt1", img: "apprt1.jpg" }, { id: "5", name: "apprt1", img: "apprt2.jpg" }, { id: "6", name: "apprt1", img: "apprt3.jpg" }, { id: "4", name: "apprt1", img: "apprt1.jpg" }, { id: "5", name: "apprt1", img: "apprt2.jpg" }, { id: "6", name: "apprt1", img: "apprt3.jpg" }, { id: "4", name: "apprt1", img: "apprt1.jpg" }, { id: "5", name: "apprt1", img: "apprt2.jpg" }]
 
 }
